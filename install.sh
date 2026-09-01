6680ca7dcd33145ad46d0d1472cd3d655e787e69
Jure Pustoslemšek <jure.pustoslemsek@fri.uni-lj.si>
2026-09-01 14:20:20 +0200

    quality of life and fixed install script

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ mkdir -p /usr/local/share/home_clone
 } > /dev/null 2>&1
 
 cp pam_script_ses_open /usr/share/libpam-script
+chown root:root /usr/share/libpam-script/pam_script_ses_open
+chmod u+x /usr/share/libpam-script/pam_script_ses_open
 cp home_clone home_clone_clean home_clone_fix /usr/local/bin
+chown root:root /usr/local/bin/home_clone /usr/local/bin/home_clone_clean /usr/local/bin/home_clone_fix
 cp share/* /usr/local/share/home_clone
+chown root:root -R /usr/local/share/home_clone
 
