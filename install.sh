2d5837edb0ac8ecfe3608869d466dc2054caf769
Jure Pustoslemšek <jure.pustoslemsek@fri.uni-lj.si>
2026-09-04 16:09:55 +0200

    make logs world-writable

--- install.sh
+++ install.sh
@@ -9,8 +9,13 @@ mkdir -p /usr/local/share/home_clone
 cp pam_script_ses_open /usr/share/libpam-script
 chown root:root /usr/share/libpam-script/pam_script_ses_open
 chmod u+x /usr/share/libpam-script/pam_script_ses_open
+
 cp home_clone home_clone_clean home_clone_fix /usr/local/bin
 chown root:root /usr/local/bin/home_clone /usr/local/bin/home_clone_clean /usr/local/bin/home_clone_fix
+
 cp share/* /usr/local/share/home_clone
 chown root:root -R /usr/local/share/home_clone
+
+touch /var/log/home_clone.log
+chmod o+w /var/log/home_clone.log
 
